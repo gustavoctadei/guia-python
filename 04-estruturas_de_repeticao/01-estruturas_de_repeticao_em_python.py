9fc18cdf2e61b320145008b437bffe6287743568
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-13 17:50:37 -0400

    aula sobre for e while concluida

---
+++ 04-estruturas_de_repeticao/01-estruturas_de_repeticao_em_python.py
@@ -0,0 +1,12 @@
+# Loops com for e while
+
+# Loop com for
+nomes = ["Pedro", "João", "Leticia"]
+for nome in nomes:
+    print(nome)
+
+# Loop com while
+contador = 0
+while contador < 5:
+    print(contador)
+    contador += 1
