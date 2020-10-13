942198d10b5bae4c39722cc09d1af9e299d9283c
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-13 17:23:12 -0400

    aula sobre estrutura condicional if-else concluida

---
+++ 03-estruturas_condicionais/02-estrutura_condicional_if-else.py
@@ -0,0 +1,15 @@
+idade = 18
+if idade >= 18:
+    print('maior de idade')
+else:
+    print('menor de idade')
+
+idade = 18
+if idade < 12:
+    print('crianca')
+elif idade < 18:
+    print('adolescente')
+elif idade < 60:
+    print('adulto')
+else:
+    print('idoso')
