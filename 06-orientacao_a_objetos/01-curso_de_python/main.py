dee695eb08210d11742cf786d816f78e01600e93
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-14 10:05:14 -0400

    curso de python | 4 - realizar os calculos da aplicacao

--- 06-orientacao_a_objetos/01-curso_de_python/main.py
+++ 06-orientacao_a_objetos/01-curso_de_python/main.py
@@ -2,4 +2,12 @@ largura: float = float(input("Qual a largura do cômodo? "))
 altura: float = float(input("Qual a profundidade do cômodo? "))
 profundidade: float = 2.9
 
-print("A área das paredes é:", (2 * (largura + profundidade) * altura))
+area_paredes: float = 2 * (largura + profundidade) * altura
+
+print("A área das paredes é:", area_paredes)
+
+area_teto: float = largura * profundidade
+
+print("A Área do teto é:", area_teto)
+
+print("A litragem de tinta necessária é:", (area_paredes + area_teto) / 10)
