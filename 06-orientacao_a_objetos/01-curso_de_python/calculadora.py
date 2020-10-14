7775e4d73c5be1444479c1f60dea50038eb0fe6a
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-14 11:12:13 -0400

    curso de python | 8 - privacidade de atributos

--- 06-orientacao_a_objetos/01-curso_de_python/calculadora.py
+++ 06-orientacao_a_objetos/01-curso_de_python/calculadora.py
@@ -1,14 +1,15 @@
 class Calculadora:
-    area_paredes: float
-    area_teto: float
+    # Para os atributos serem privados, devemos declará-los com __ no início do nome
+    __area_paredes: float
+    __area_teto: float
 
-    def calcular_area_paredes(self, largura, profundidade, altura):
-        self.area_paredes = 2 * (largura + profundidade) * altura
-        return self.area_paredes
+    def calcular_area_paredes(self, comodo):
+        self.__area_paredes = 2 * (comodo.largura + comodo.profundidade) * comodo.altura
+        return self.__area_paredes
 
-    def calcular_area_teto(self, largura, profundidade):
-        self.area_teto = largura * profundidade
-        return self.area_teto
+    def calcular_area_teto(self, comodo):
+        self.__area_teto = comodo.largura * comodo.profundidade
+        return self.__area_teto
 
     def calcular_litragem_necessaria(self):
-        return ((self.area_paredes + self.area_teto) / 10)
+        return (self.__area_paredes + self.__area_teto) / 10
--- 06-orientacao_a_objetos/01-curso_de_python/main.py
+++ 06-orientacao_a_objetos/01-curso_de_python/main.py
@@ -10,10 +10,10 @@ comodo = Comodo(
 
 # calculadora.area_paredes: float = 2 * (largura + profundidade) * altura
 
-print("A área das paredes é:", calculadora.calcular_area_paredes(comodo.largura, comodo.profundidade, comodo.altura))
+print("A área das paredes é:", calculadora.calcular_area_paredes(comodo))
 
 # calculadora.area_teto: float = largura * profundidade
 
-print("A Área do teto é:", calculadora.calcular_area_teto(comodo.largura, comodo.profundidade))
+print("A Área do teto é:", calculadora.calcular_area_teto(comodo))
 
 print("A litragem de tinta necessária é:", calculadora.calcular_litragem_necessaria())
