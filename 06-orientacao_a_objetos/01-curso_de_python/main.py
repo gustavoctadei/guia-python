e706498c2efd2757aeccd53af96c7c07acf4f3f6
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-14 10:23:30 -0400

    curso de python | 6 - criacao de metodos

--- 06-orientacao_a_objetos/01-curso_de_python/main.py
+++ 06-orientacao_a_objetos/01-curso_de_python/main.py
@@ -6,12 +6,12 @@ largura: float = float(input("Qual a largura do cômodo? "))
 altura: float = float(input("Qual a profundidade do cômodo? "))
 profundidade: float = 2.9
 
-calculadora.area_paredes: float = 2 * (largura + profundidade) * altura
+# calculadora.area_paredes: float = 2 * (largura + profundidade) * altura
 
-print("A área das paredes é:", calculadora.area_paredes)
+print("A área das paredes é:", calculadora.calcular_area_paredes(largura, profundidade, altura))
 
-calculadora.area_teto: float = largura * profundidade
+# calculadora.area_teto: float = largura * profundidade
 
-print("A Área do teto é:", calculadora.area_teto)
+print("A Área do teto é:", calculadora.calcular_area_teto(largura, profundidade))
 
-print("A litragem de tinta necessária é:", (calculadora.area_paredes + calculadora.area_teto) / 10)
+print("A litragem de tinta necessária é:", calculadora.calcular_litragem_necessaria())
