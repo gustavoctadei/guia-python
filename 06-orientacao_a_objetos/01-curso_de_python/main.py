8e232bd6d631874d3f54dbb3ac3c186e453d59d4
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-14 09:51:44 -0400

    curso de python | 2 - comecando o projeto

---
+++ 06-orientacao_a_objetos/01-curso_de_python/main.py
@@ -0,0 +1,11 @@
+largura = 5
+altura = 3
+profundidade = 3
+
+# Declarar as variáveis desta forma, caso queira especificar o tipo
+# largura: float = 5
+# altura: float = 3
+# profundidade: float = 3
+
+print(2 * (largura + profundidade) * altura)
+print(largura * profundidade)
