dfbf46d5d2b5784f17854fbe6c6e21512d2a5324
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-14 11:25:22 -0400

    curso de python | 9 - propriedades e excecoes

--- 06-orientacao_a_objetos/01-curso_de_python/comodo.py
+++ 06-orientacao_a_objetos/01-curso_de_python/comodo.py
@@ -1,11 +1,41 @@
 class Comodo:
-    largura: float
-    profundidade: float
-    altura: float
+    __largura: float
+    __profundidade: float
+    __altura: float
 
     # Construtor da Classe
     # Não é necessário declarar os atributos previamente
     def __init__(self, largura, profundidade):
-        self.largura = float(largura)
-        self.profundidade = float(profundidade)
-        self.altura = 2.9
+        self.largura = largura
+        self.profundidade = profundidade
+        self.__altura = 2.9
+
+    @property
+    def largura(self):
+        return self.__largura
+
+    @property
+    def profundidade(self):
+        return self.__profundidade
+
+    @property
+    def altura(self):
+        return self.__altura
+
+    @largura.setter
+    def largura(self, largura):
+        try:
+            self.__largura = float(largura)
+
+        except Exception:
+            print("O valor da largura informado é inválido")
+            exit()
+
+    @profundidade.setter
+    def profundidade(self, profundidade):
+        try:
+            self.__profundidade = float(profundidade)
+
+        except Exception:
+            print("O valor da profundidade informado é inválido")
+            exit()
