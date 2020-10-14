537fbc7256fd203ca7918c7fe4d6ce34e10a4134
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-14 11:31:58 -0400

    curso de python | 10 - estrutura condicional

--- 06-orientacao_a_objetos/01-curso_de_python/calculadora.py
+++ 06-orientacao_a_objetos/01-curso_de_python/calculadora.py
@@ -12,4 +12,8 @@ class Calculadora:
         return self.__area_teto
 
     def calcular_litragem_necessaria(self):
+        if self.__area_paredes <= 0 or self.__area_teto <= 0:
+            print("Não foi possível calcular a litragem com os valores informados")
+            exit()
+
         return (self.__area_paredes + self.__area_teto) / 10
