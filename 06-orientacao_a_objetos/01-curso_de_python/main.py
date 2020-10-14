cb3a30e5cf1ce73148724b544beac4f1530cc9c5
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-14 10:16:04 -0400

    curso de python | 5 - orientacao a objeto

--- 06-orientacao_a_objetos/01-curso_de_python/main.py
+++ 06-orientacao_a_objetos/01-curso_de_python/main.py
@@ -1,13 +1,17 @@
+from calculadora import Calculadora
+
+calculadora = Calculadora()
+
 largura: float = float(input("Qual a largura do cômodo? "))
 altura: float = float(input("Qual a profundidade do cômodo? "))
 profundidade: float = 2.9
 
-area_paredes: float = 2 * (largura + profundidade) * altura
+calculadora.area_paredes: float = 2 * (largura + profundidade) * altura
 
-print("A área das paredes é:", area_paredes)
+print("A área das paredes é:", calculadora.area_paredes)
 
-area_teto: float = largura * profundidade
+calculadora.area_teto: float = largura * profundidade
 
-print("A Área do teto é:", area_teto)
+print("A Área do teto é:", calculadora.area_teto)
 
-print("A litragem de tinta necessária é:", (area_paredes + area_teto) / 10)
+print("A litragem de tinta necessária é:", (calculadora.area_paredes + calculadora.area_teto) / 10)
